9823a140e278fc2ff20ef06d4d4f7545f63d1360
Stephen Newell <sjnewell@gmail.com>
2018-07-08 17:56:11 -0600

    Housekeeping.
    * setup.py has long_description, url, and classifiers
    * add CONTRIBUTIN and ISSUE_TEMPLATE files

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup, find_packages
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
     name="dev-pipeline-build-order",
     version="0.2.0",
@@ -29,5 +32,17 @@ setup(
 
     author="Stephen Newell",
     description="build-order command for dev-pipeline",
-    license="BSD-2"
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    license="BSD-2",
+    url="https://github.com/dev-pipeline/dev-pipeline-build-order",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Plugins",
+        "Framework :: dev-pipeline",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development",
+        "Topic :: Utilities"
+    ]
 )
